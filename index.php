780d1184267b0bc6d751a8f8561621a8f9ba8a4e
Aqib <aqibr777@gmail.com>
2022-07-26 00:03:45 +0500

    03 Your first route

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
 require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function() {
+    echo 'Home';
+});
+
+$app->get('/users', function() {
+    echo 'Users';
+});
+
+$app->run();
