21260a7e37841af6bfffc5ff029ca0c6ad407399
Aqib <aqibr777@gmail.com>
2022-07-31 20:51:49 +0500

    20. Basic Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
-use App\Models\User;
+use App\Controllers\TopicController;
 
-$app->get('/', function() {
-    $user = new User;
-    var_dump($user);
+// $app->get('/topics', '\App\Controllers\TopicController:index');
+// $app->get('/topics/{id}', '\App\Controllers\TopicController:show');
+
+// $app->get('/topics', TopicController::class . ':index');
+// $app->get('/topics/{id}', TopicController::class . ':show');
+
+$app->group('/topics', function() {
+    $this->get('', TopicController::class . ':index');
+    $this->get('/{id}', TopicController::class . ':show');
 });
