b5c72f05ba3a4abb0582bfddf2280fc05e1b9a9f
Aqib <aqibr777@gmail.com>
2022-08-07 00:58:24 +0500

    24. Helpful model methods

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controllers;
 
+use PDO;
+use App\Models\User;
+
 class UserController extends Controller
 {
 
     public function index($request, $response)
     {
-      return 'User index';
+       $users = $this->c->db->query("SELECT * FROM users")->fetchAll(PDO::FETCH_CLASS, User::class);
+
+       return $this->c->view->render($response, 'users/index.twig', compact('users'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,5 +3,24 @@
 namespace App\Models;
 
 class User {
-    
+
+    public function fullName()
+    {
+        if($this->last_name === null) 
+        {
+            return $this->first_name;
+        }
+
+        return "{$this->first_name} {$this->last_name}";
+    }
+
+    public function getFormattedBalance()
+    {
+        if($this->balance == 0)
+        {
+            return 'Zero funds';
+        }
+        
+        return '$' . number_format($this->balance, 2);
+    }
 }
